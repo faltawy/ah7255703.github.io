54d4158a99c461eee793876c5d6acb04f993cdfe
Ahmed Hassan <ah7255703@gmail.com>
2023-03-02 14:08:14 +0200

    my first commit.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return <main className="container mx-auto">
+
+  </main>;
+}
+
+export default App;
---
+++ src/SlicedImage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface Props extends React.PropsWithChildren {}
+
+export function SlicedImage(props:Props) {
+    return <div></div>
+}
