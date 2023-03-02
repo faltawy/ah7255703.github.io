0f8d33c6c1240cc68d0637dcb26e5f54e8315737
Ahmed Hassan <ah7255703@gmail.com>
2023-03-02 14:25:28 +0200

    vite build

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-
+import React from "react";
 
 function App() {
-  return <main className="container mx-auto">
-
-  </main>;
+  return (
+    <main className="container mx-auto">
+      <h2 className="text-xl">Hell Yes,it is working...</h2>
+    </main>
+  );
 }
 
 export default App;
